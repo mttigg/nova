3b39bc4de79b6692ecab873dccfd1def5bb28a2f
mtti-g <matt.greff@meetgradient.com>
2022-02-18 16:10:33 +0000

    add revamped latest tmux install

--- commands/vim/install.sh
+++ commands/vim/install.sh
@@ -12,7 +12,20 @@ cp ~/git/gruvbox/colors/gruvbox.vim ~/.vim/colors/
 echo "Installing LanguageServers..."
 npm i -g typescript
 echo "Installing tmux..."
-sudo yum install tmux
+sudo yum install ncurses
+sudo yum install ncurses-devel
+sudo yum install libevent
+sudo yum install libevent-devel
+sudo yum install automake
+sudo yum install autoconf
+sudo yum install gcc
+cd ~/git
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure
+make && sudo make install
+cd ~
 echo "Setting up tmux..."
 cp ~/.nova/.tmux.conf ~/.tmux.conf
 echo "-----Vim-Is-Now-Spruced-----\n\n"
