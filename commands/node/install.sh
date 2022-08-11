db0878418d6cd241417a95b0628a341228ef4214
mtti-g <matt.greff@meetgradient.com>
2022-08-11 00:26:31 +0000

    use default

--- commands/node/install.sh
+++ commands/node/install.sh
@@ -3,3 +3,4 @@ talk "Fetching Node Version Manager..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 talk "Installing Node @16.15.0..."
 nvm install 16.15.0
+nvm default 16.15.0
