aea78803252634888d56bb4e0e4e446ad467ab89
mtti-g <matt.greff@meetgradient.com>
2022-08-11 00:26:13 +0000

    chore add python vars to session

--- commands/python/install.sh
+++ commands/python/install.sh
@@ -26,7 +26,14 @@ talk "installing pyenv"
 cd ~/git
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+talk "adding variables to session"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 talk "adding variables to bashrc"
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
